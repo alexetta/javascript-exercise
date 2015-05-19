571bed4c59c2900705d28406044a73d75c05dbb5
Alexandra Tresaco <alexandratresaco@gmail.com>
2015-05-19 18:56:39 +0200

    added - coment

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -20,31 +20,59 @@ $(document).ready(function(){
 	$('#menu-container nav > ul > li > a').click(clickResaltado);
 
 
-	$(window).resize(function){
 
-		var hamburguesa = $('#mobile-button')
-		var menu = $('#menu-container')
-		var cruz = $('#mobile-close')
+
+	
+
+	var hamburguesa = $('#mobile-button')
+	var menu = $('#menu-container')
+	var cruz = $('#mobile-close')
 		
-		if ($(window).width() <= 990) {
 
-			hamburguesa.click(function(e) {
-				e.preventDefault();
-					menu.show();
-					hamburguesa.hide();
-					cruz.show();
-			});
+	hamburguesa.click(function(e) {
+		e.preventDefault();
+		menu.show();
+		hamburguesa.hide();
+		cruz.show();
+	});
 
-			cruz.click(function(e) {
-				e.preventDefault();
-					cruz.hide();
-					menu.hide();
-					hamburguesa.show();	
-			});
-		};else{
-			hamburguesa.hide();
-			cruz.hide();
-		};
+	cruz.click(function(e) {
+		e.preventDefault();
+		cruz.hide();
+		menu.hide();
+		hamburguesa.show();	
+		
+		
 	};
+	
+
+	//He probado que cuando la ventana sea más pequeña que 990px aplique las funciones siguientes y sino que me esconda la cruz y la hamburguesa
+
+	// $(window).resize(function){
+
+	// 	var hamburguesa = $('#mobile-button')
+	// 	var menu = $('#menu-container')
+	// 	var cruz = $('#mobile-close')
+		
+	// 	if ($(window).width() <= 990) {
+
+	// 		hamburguesa.click(function(e) {
+	// 			e.preventDefault();
+	// 				menu.show();
+	// 				hamburguesa.hide();
+	// 				cruz.show();
+	// 		});
+
+	// 		cruz.click(function(e) {
+	// 			e.preventDefault();
+	// 				cruz.hide();
+	// 				menu.hide();
+	// 				hamburguesa.show();	
+	// 		});
+	// 	};else{
+	// 		hamburguesa.hide();
+	// 		cruz.hide();
+	// 	};
+	// };
 });
 
